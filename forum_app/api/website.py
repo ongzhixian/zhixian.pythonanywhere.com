c70734df02bb29cc528f14600c73209f1a2c9572
Ong Zhixian <zhixian@hotmail.com>
2021-02-27 18:36:23 +0800

    test

--- forum_app/api/website.py
+++ forum_app/api/website.py
@@ -59,6 +59,10 @@ def api_datetime():
 
         x = root_logger.handlers[0]
         logging.info(x.name)
+        logging.info(x.level)
+        y = root_logger.handlers[1]
+        logging.info(y.name)
+        logging.info(y.level)
 
         #logging.info(str(dir(logging.getLogger().handlers[0])))
 
