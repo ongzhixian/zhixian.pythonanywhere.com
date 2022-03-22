d39401eed55129fc3440c9a1b35b3ca35f2abc99
zhixian <zhixian@hotmail.com>
2022-03-22 22:37:59 +0800

    add db

--- forum_app/api/__init__.py
+++ forum_app/api/__init__.py
@@ -2,6 +2,6 @@
 # Define package composition
 ################################################################################
 
-__all__ = ["website", "test"]
+__all__ = ["website", "test", "db"]
 
 # ZX: There's a better way to do this; see other source code from elsewhere
---
+++ forum_app/api/db.py
@@ -0,0 +1,18 @@
+# 
+################################################################################
+# Modules and functions import statements
+################################################################################
+
+import logging
+
+from forum_app import app
+from forum_app.modules.forum_db import ForumDb
+
+
+@app.route('/api/db/init', methods=['GET', 'POST'])
+def api_db_init():
+    mydb = ForumDb()
+    mydb.init_new_tables()
+    logging.info("ForumDb initialized.")
+    return "All databases initialized."
+    
