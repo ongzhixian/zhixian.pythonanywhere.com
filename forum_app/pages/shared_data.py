9398aa4f07b5ca70b9501ba5d12202f539e4661c
Zhixian Ong <zhixian.ong@ti8m.ch>
2022-07-15 11:05:31 +0800

    add mic

--- forum_app/pages/shared_data.py
+++ forum_app/pages/shared_data.py
@@ -396,3 +396,64 @@ def shared_data_currency_post():
     action_map[action]()
 
     return redirect(url_for('shared_data_currency'))
+
+
+@app.route('/shared-data/mic')
+def shared_data_mic():
+    """Web page at '/shared-data/mic'"""
+    # from forum_app.databases.forum_database import MySqlDataProvider
+    # db = MySqlDataProvider('forum')
+    return render_template('shared_data/shared_data_mic.html', isin_list=None)
+    # Insert mic data
+
+
+def get_mic_from_csv_file():
+    mic_csv_file_path = path.join(app_path, 'data', 'comma_separated_values', 'iso-10383-market-identifier-codes.csv')
+    mic_data = []
+
+    # 00 - "COUNTRY",
+    # 01 - "ISO COUNTRY CODE (ISO 3166)",
+    # 02 - "MIC",
+    # 03 - "OPERATING MIC",
+    # 04 - "O/S",
+    # 05 - "NAME-INSTITUTION DESCRIPTION",
+    # 06 - "ACRONYM",
+    # 07 - "CITY",
+    # 08 - "WEBSITE",
+    # 09 - "STATUS DATE",
+    # 10 - "STATUS",
+    # 11 - "CREATION DATE",
+    # 12 - "COMMENTS"
+
+    import csv
+    with open(mic_csv_file_path, "r", encoding="utf8") as infile:
+        csv_reader = csv.reader(infile)
+        next(csv_reader, None)  # skip the headers
+        for row in csv_reader:
+            mic_data.append((row[0], row[1], row[2], row[3], row[5], row[6], row[7], row[8], row[10], row[12]))
+    return mic_data
+
+def insert_mic_data():
+    mic_list = get_mic_from_csv_file()
+    from forum_app.databases.forum_database import MySqlDataProvider
+    db = MySqlDataProvider('forum')
+    sql = """INSERT INTO market_identifier (country_name, country_code2, code, operating_mic, name, short_name, city, url, status, comments)  VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+    db.execute_many(sql, mic_list)
+
+
+@app.route('/shared-data/mic', methods=['POST'])
+def shared_data_mic_post():
+    """Web page at '/shared-data/currency'"""
+    logging.info("POST to shared_data_currency_post")
+
+    action_map = {
+        'Insert mic data': insert_mic_data
+    }
+
+    if 'action' not in request.form:
+        return
+    
+    action = request.form['action']
+    action_map[action]()
+
+    return redirect(url_for('shared_data_mic'))
