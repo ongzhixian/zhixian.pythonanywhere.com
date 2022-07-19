b998eb2ea5c226a52b7454bc752792ba82bc6e04
Zhixian Ong <zhixian.ong@ti8m.ch>
2022-07-19 11:39:49 +0800

    udpate

--- forum_app/api/test.py
+++ forum_app/api/test.py
@@ -6,15 +6,22 @@
 import json
 import logging
 
-from os import environ
+from os import environ, path
 from datetime import datetime
 from time import time
 
 from flask import request, make_response, abort
 
-from forum_app import app
+from forum_app import app, app_path
 from forum_app.databases.forum_database import ForumDatabase
 
+@app.route('/api/test/oda', methods=['GET', 'POST'])
+def api_test_oda():
+    logging.info("In api_test_mysql()")
+    json_file_path = path.join(app_path, 'data', 'json', 'oda-instruments.json')
+    with open(json_file_path, 'r', encoding='utf-8') as json_file:
+        data = json.load(json_file) # Load the JSON data
+    return json.dumps(data)
 
 
 @app.route('/api/test/mysql', methods=['GET', 'POST'])
