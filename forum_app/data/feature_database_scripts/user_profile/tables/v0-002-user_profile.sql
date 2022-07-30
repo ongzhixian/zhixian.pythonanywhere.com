0433804ef48f723e9006c6d628d884b78e0ddfb7
zhixian <zhixian@hotmail.com>
2022-07-30 17:08:40 +0800

    okok

--- forum_app/data/feature_database_scripts/user_profile/tables/v0-002-user_profile.sql
+++ forum_app/data/feature_database_scripts/user_profile/tables/v0-002-user_profile.sql
@@ -3,8 +3,11 @@ SET sql_notes = 0; -- suppress warnings
 CREATE TABLE IF NOT EXISTS `user_profile` (
 	`id`					INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	`login_id` 				VARCHAR(50) NOT NULL,
-	`first_name` 			VARCHAR(50) NOT NULL,
-	`last_name` 			VARCHAR(50) NOT NULL,
+	`first_name` 			VARCHAR(128) NOT NULL,
+	`last_name` 			VARCHAR(128) NOT NULL,
+	`email` 				VARCHAR(128) NOT NULL,
+	`portrait_filename`		VARCHAR(128) NOT NULL,
+	`background_filename`	VARCHAR(128) NOT NULL,
 	`cre_dt` 				DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
 	`upd_dt` 				DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
 	PRIMARY KEY (`id`)
