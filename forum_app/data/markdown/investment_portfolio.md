fa7edfd5f41bacf882b07101000725a3527a6c03
Zhixian Ong <zhixian.ong@ti8m.ch>
2022-08-04 15:53:18 +0800

    update

---
+++ forum_app/data/markdown/investment_portfolio.md
@@ -0,0 +1,25 @@
+# Personal Investment Management System (PIMS)
+
+
+
+## Screens
+
+- Portfolio list screen
+- Portfolio detail screen
+- Portfolio edit screen
+- Portfolio create screen
+- Portfolio delete screen
+
+Portfolio theme/goal
+    - add position
+    - add transaction
+
+
+- Portfolio transaction screen
+- Portfolio transaction detail screen
+- Portfolio transaction edit screen
+- Portfolio transaction delete screen
+- Portfolio transaction create screen
+- Portfolio transaction delete screen
+- Portfolio transaction create screen
+
--- forum_app/pages/wms_location.py
+++ forum_app/pages/wms_location.py
@@ -2,6 +2,7 @@ from flask import render_template, redirect, url_for
 from forum_app import app
 
 from forum_app.features.wms import WmsFeature
+from forum_app.features.wms_location import WmsLocationFeature
 
 @app.route('/wms/location/')
 def wms_location_get():
@@ -14,12 +15,15 @@ def wms_location_list():
     """Web page at /wms/location/list"""
     breadcrumbs = [
         { 'href' : '/wms/dashboard', 'text': 'WMS' },
-        { 'href' : None, 'text': 'Location' },
-        { 'href' : None, 'text': 'List' }
+        { 'href' : None, 'text': 'Location List' }
     ]
     wms = WmsFeature()
+    wms_location = WmsLocationFeature()
+    location_list = wms_location.get_location_list()
+
     dashboard_list = wms.get_dashboard_list()
     return render_template(
         'wms/wms_location_list.html',
         breadcrumbs=breadcrumbs,
-        dashboard_list=dashboard_list)
+        dashboard_list=dashboard_list,
+        location_list=location_list)
