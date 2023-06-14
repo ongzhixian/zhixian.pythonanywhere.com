da16a2253b18ae5ff78df689a14b2370d8d4fadd
zhixian <zhixian@hotmail.com>
2023-06-14 23:34:15 +0800

    update

--- gasmgmt/vite.config.js
+++ gasmgmt/vite.config.js
@@ -1,7 +1,20 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: '/gasmgmt/',
+    plugins: [react()],
+    build: {
+        outDir: "../forum_app/wwwroot/gasmgmt",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                app1: resolve(__dirname, 'app1/index1.html'),
+                app2: resolve(__dirname, 'app2/index2.html'),
+            },
+        },
+    }
 })
