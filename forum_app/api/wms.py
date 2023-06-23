1b7108d5b6b4e75e2b028aefe1605532125f4428
zhixian <zhixian@hotmail.com>
2023-06-24 01:51:50 +0800

    test

--- forum_app/api/wms.py
+++ forum_app/api/wms.py
@@ -3,14 +3,68 @@ from flask import render_template, session, request, redirect, url_for
 from forum_app import app
 from forum_app.features.logging import log
 import mysql.connector
+from forum_app import app_secrets
 
-
+db_settings = app_secrets["MYSQL"]['forum']
 
 @app.route('/api/wms/customer', methods=['GET'])
 def api_get_wms_customer():
     result = "asd"
-    # cnx = mysql.connector.connect(user='scott', password='password', host='127.0.0.1', database='employees')
-    # cnx.close()
+
+    with mysql.connector.connect(user=db_settings['USERNAME'], password=db_settings['PASSWORD'], host=db_settings['HOST'], database=db_settings['DATABASE']) as connection, connection.cursor() as cursor:
+        # query = ("SELECT first_name, last_name, hire_date FROM employees ")
+        # query = "SELECT first_name, last_name, hire_date FROM employees "
+        query = "SELECT id, name FROM wms_customer "
+        cursor.execute(query)
+
+        #for (first_name, last_name, hire_date) in cursor:
+        for (id, name) in cursor:
+            # print("{}, {} was hired on {:%d %b %Y}".format(
+            #     last_name, first_name, hire_date))
+            # result = {
+            #     last_name: last_name, 
+            #     first_name: first_name, 
+            #     hire_date: hire_date
+            # }
+            result = {
+                id: id, 
+                name: name, 
+            }
+
+        # cursor.close()
+        # connection.close()
+
+    if result is None:
+        return "Bad request", 400
+    else:
+        return str(result)
+
+
+
+
+def sample_api_get_wms_customer():
+    result = "asd"
+
+    connection = mysql.connector.connect(user=db_settings['USERNAME'], password=db_settings['PASSWORD'], host=db_settings['HOST'], database=db_settings['DATABASE'])
+    cursor = connection.cursor()
+
+    # query = ("SELECT first_name, last_name, hire_date FROM employees "
+    #         "WHERE hire_date BETWEEN %s AND %s")
+
+    # hire_start = datetime.date(1999, 1, 1)
+    # hire_end = datetime.date(1999, 12, 31)
+
+    # cursor.execute(query, (hire_start, hire_end))
+
+    query = ("SELECT first_name, last_name, hire_date FROM employees ")
+    cursor.execute(query)
+
+    for (first_name, last_name, hire_date) in cursor:
+        print("{}, {} was hired on {:%d %b %Y}".format(
+            last_name, first_name, hire_date))
+
+    cursor.close()
+    connection.close()
 
     if result is None:
         return "Bad request", 400
